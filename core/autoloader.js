8b2144214fdc4c4e3910c1884bb7c63e54b28746
Miguel <suarezsanzmiguel@gmail.com>
2025-11-13 18:47:53 +0100

    Se ha añadido la estructura actualizada del framework y se esta empezando a hacer el header

--- core/autoloader.js
+++ core/autoloader.js
@@ -1,57 +1,149 @@
-async function renderize(ruta, etiqueta) {
-    fetch(ruta)
-    .then(response => response.text())
-    .then(data => document.querySelector(etiqueta).innerHTML = data); 
-}
+class FrameworkUtils {
 
-async function loadView(target) {
-    let intervalo = loadInit();
-    let route = `src/${target}/${target}`;
-    loadCSS(`${route}.css`)
-    .then(loadScript(`${route}.js`))
-    .then(renderize(`${route}.html`, "main"))
-    .then(clearInterval(intervalo));
-}
+    routeError = "error404";
+    cssTag = "#cssRender";
+    jsTag = "#jsRender";
+    htmlTag = "main";
 
-async function loadCSS(target) {
-    let link = document.querySelector('#cssRender');
-    link.href = target;
-}
+    indexName = "index";
+    headerName = "header";
+    footerName = "footer";
 
-async function loadScript(target) {
-    let script = document.querySelector('#jsRender');
-    script.src = target;
-}
+    urlParams = new URLSearchParams(window.location.search);
 
+    constructor() {
 
-async function autoInit() {
-    renderize("includes/html/header.html","header");
-    renderize("includes/html/footer.html","footer");
-    var urlParams = new URLSearchParams(window.location.search);
+        this.autoInit();
 
-    if (urlParams.has("target")) {
-        loadView(urlParams.get("target"));
-    } else {
-        loadView("index");
     }
-}
 
-async function loadInit() {
-    return renderize("includes/html/loading.html","main")
-    .then(()=>{
-        return setInterval(()=>{
-        
-            let puntos = document.querySelector(".puntos");
-            let puntosText = puntos.innerHTML;
-            let numMaxPuntos = 3;
+    async loadHtml(target, tag) {
 
-            if (puntosText.length != numMaxPuntos) {
-                puntos.innerHTML += ".";
-            } else {
-                puntos.innerHTML = "";
+        target += ".html";
+
+        if (!this.checkRoute(target)) {
+            console.error(`FATAL ERROR, error HTML not found: ${target}`);
+            return;
+        }
+
+        fetch(target)
+            .then(response => response.text())
+            .then(data => document.querySelector(tag).innerHTML = data);
+
+    }
+
+    async loadCss(target, tag) {
+
+        target += ".css";
+
+        if (!this.checkRoute(target)) {
+            console.error(`FATAL ERROR, error CSS not found: ${target}`);
+            return;
+        }
+
+        let link = document.querySelector(tag);
+        link.href = target;
+
+    }
+
+    async loadScript(target, tag) {
+
+        target += ".js";
+
+        if (!this.checkRoute(target)) {
+            console.error(`FATAL ERROR, error Script not found: ${target}`);
+            return;
+        }
+
+        let script = document.querySelector(tag);
+        script.src = target;
+
+    }
+
+    async renderize(target) {
+
+        let interval = await this.loandingProcess();
+        let route = `src/${target}/${target}`;
+        let comprobar = await this.checkRoute(`src/${target}`);
+
+        if (!comprobar) {
+
+            if (target == this.routeError) {
+                console.error(`FATAL ERROR, error route not found: ${this.routeError}`);
+                return;
             }
-        
-        },500)
 
-    });
-}
+            clearInterval(interval);
+            this.renderize(this.routeError);
+            return;
+        }
+
+        this.loadCss(route, this.cssTag)
+            .then(await this.loadScript(route, this.jsTag))
+            .then(await this.loadHtml(route, this.htmlTag))
+            .then(clearInterval(interval));
+
+    }
+
+    async checkRoute(route) {
+        try {
+
+            const response = await fetch(route);
+
+            if (!response.ok) {
+                return false;
+            }
+            return true;
+
+        } catch (error) {
+            return false;
+        }
+
+    }
+
+    async autoInit() {
+
+        this.loadHtml(`includes/html/${this.headerName}`, "header");
+        this.loadHtml(`includes/html/${this.footerName}`, "footer");
+
+        if (this.urlParams.has("target")) {
+
+            this.renderize(urlParams.get("target"));
+
+        } else {
+
+            this.renderize(this.indexName);
+
+        }
+
+    }
+
+    async loandingProcess() {
+        let intervalo = null;
+
+        await this.loadHtml("includes/html/loading", "main")
+
+            .then(() => {
+
+                intervalo = setInterval(() => {
+
+                    let puntos = document.querySelector(".puntos");
+                    let puntosText = puntos.innerHTML;
+                    let numMaxPuntos = 3;
+
+                    if (puntosText.length != numMaxPuntos) {
+                        puntos.innerHTML += ".";
+                    } else {
+                        puntos.innerHTML = "";
+                    }
+
+                }, 500);
+
+            });
+
+        return intervalo;
+    }
+
+};
+
+let frameworkUtils = new FrameworkUtils();
