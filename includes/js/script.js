7b34efd8e90283d4f06c666dcb94518e6d773859
dam2 <Miguel Suarez Sanz>
2025-12-18 16:25:47 +0100

    Se ha mejorado buscador avanzado, se ha creado la vista busqueda y se ña añadido metodos del popup

--- includes/js/script.js
+++ includes/js/script.js
@@ -1,6 +1,28 @@
 function buscar() {
     let buscador = document.querySelector('#buscador');
-    console.log(buscador.innerHTML);
-    var busqueda = buscador.innerHTML.trim();
-    frameworkUtils.renderize("buscador");
+    busqueda = buscador.value.trim();
+    console.log(busqueda);
+    frameworkUtils.renderize("busqueda");
 }
+
+function openPopup(innerHtml) {
+    let black = document.querySelector("#black");
+    black.style.height = "100vh";
+    black.style.width = "100vw";
+
+    let popup = document.querySelector("#popup");
+    popup.style.top = "10vh";
+    popup.innerHTML = innerHtml;
+}
+
+function closePopup() {
+    let black = document.querySelector("#black");
+    black.style.height = "0";
+    black.style.width = "0";
+
+    document.querySelector("#popup").style.top = "-1000px";
+}
+
+document.querySelector("#black").addEventListener('click',closePopup);
+var busquedaAvanzada = [];
+var busqueda = "";
